18a4d42297ca820f326afb4e1410e6bc0388e13f
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-10 19:30:47 +0300

    56. Sayfa Yükleniyor Yapımı

--- src/App.js
+++ src/App.js
@@ -4,36 +4,47 @@ import "./App.css"
 class App extends Component {
 
   state = {
-    users: []
+    users: [],
+    isLoading: true
   };
 
   componentDidMount = () => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(data => data.json()).then(vepa => {
-        console.log(vepa);
 
-        this.setState({
-          users: vepa
-        })
-      }
-      )
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then(data => data.json()).then(vepa => {
+          console.log(vepa);
+
+          this.setState({
+            users: vepa,
+            isLoading: false
+          })
+        }
+        )
+    },800);
+
   }
 
 
 
   render() {
+
+    const { isLoading } = this.state
+
     return (
       <div className='App'>
 
         <h1>Users</h1>
 
+        {isLoading ? "Loading..." : ""}
+
         {
-          this.state.users.map(user => 
-            <div key = {user.id}>
+          this.state.users.map(user =>
+            <div key={user.id}>
               {
                 user.name
               }
-               --@@
+              --@@
               {
                 user.username
               }
