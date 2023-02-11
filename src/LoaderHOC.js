39afdc63c3d11400fc4eee064b364d2083c7ad90
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-11 12:51:25 +0300

    LoaderHOC.js eklendi ve yapısı degiştirildi

---
+++ src/LoaderHOC.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class LoaderHOC = (WrappedComponent) => {
+
+    return class LoaderHOC extends Component {
+
+        render() {
+            return (
+                
+
+            );
+        }
+    }
+
+}
+
+export default LoaderHOC;
